4d5bbb650c55dd5684bcc0f5225266df6ad867a6
BengalDroid <bengaldroid@gmail.com>
2021-07-03 12:01:25 +0600

    short syntax

--- switch1.c
+++ switch1.c
@@ -7,17 +7,32 @@ int main()
 
     switch(day_number){
         case 0:
-        case 6:
-        printf("off day\n");
+        printf("Saturday\n");
+        break;
+
+        case 1:
+        printf("Sunday\n");
+        break;
+        case 2:
+        printf("Monday\n");
         break;
         
-        case 1:
-        case 2:
         case 3:
-        case 4:
-        case 5:
-        printf("workd day\n");
+        printf("Tuesday\n");
         break;
+
+        case 4:
+        printf("Wednesday\n");
+        break;
+
+        case 5:
+        printf("Thursady\n");
+        break;
+
+        case 6:
+        printf("Friday\n");
+        break;
+
         default:
         printf("Its a wrong number");
       }
